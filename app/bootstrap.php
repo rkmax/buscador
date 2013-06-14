196f9638e972892f3ad81bb04ad0b7d894dc735c
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2013-06-13 21:27:47 -0500

    agregado soporte para base datos y configuracion mediante PDO y SQlite

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,6 +9,14 @@ $app->register(new Silex\Provider\TwigServiceProvider, array(
     'twig.path' => __DIR__ . '/../src/Buscador/Vista'
 ));
 
+// Base de datos Doctrine (DBAL)
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__ . '/../cache/app.db'
+    )
+));
+
 // pagina principal
 $app->get('/', function() use ($app) {
     return $app['twig']->render('welcome.twig', array(
